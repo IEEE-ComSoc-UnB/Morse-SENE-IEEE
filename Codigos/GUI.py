c1f1d8b5f6d16f772f30b3465c2156e15fef8457
Danielvenzi <gomesvenzi@gmail.com>
2017-10-22 21:51:19 -0200

    .

--- Codigos/GUI.py
+++ Codigos/GUI.py
@@ -2,23 +2,49 @@ from Tkinter import *
 import ttk
 from PIL import ImageTk , Image
 
-def GUI():
-root = Tk()
-    root.title("MORSE")
-    path="/home/daniel/IEEE/Morse-SENE-IEEE/Codigos/images/signal.jpg"
-    img = ImageTk.PhotoImage(Image.open(path))
-    Label(root,text="Signal Interpretation System").grid(row=0,column=5)
-    Label(root,text="Signal:").grid(row=2,column=0)
-    Label(root,image=img).grid(row=3,column=0)
-    Label(root,text="Message:").grid(row=6,column=0)
-    Entry(root).grid(row=7,column=0)
-    Button(root,text="Clear").grid(row=7,column=1)
-    Label(root,text="Interpretation Period (Hz):").grid(row=2,column=7)
-    Entry(root).grid(row=3,column=7)
-    Button(root,text="Adjust").grid(row=3,column=8)
-    Label(root,text="History:").grid(row=6,column=7)
-    Entry(root).grid(row=7,column=7)
-    Button(root,text="Clear").grid(row=7,column=8)
-    root.resizable(False,False)
-root.mainloop()
+Value = True
 
+def clear_textbox():
+        entry.delete(0, END)
+
+def signal_interpreter():
+    global Value
+    while()
+
+
+
+root = Tk()
+
+root.title("MORSE - IEEE - ComSoc")
+path="/home/daniel/IEEE/Morse-SENE-IEEE/Codigos/images/signal.jpg"
+img = ImageTk.PhotoImage(Image.open(path))
+
+title = Label(root,text="SIGNAL INTERPRETATION SYSTEM")
+title.pack()
+
+image_title = Label(root,text="Guideline:")
+image_title.pack()
+image = Label(root,image=img)
+image.pack()
+
+message = Label(root,text="Message:")
+message.pack()
+entry = Entry(root)
+entry.pack(side=BOTTOM)
+#entry = Label(root,text="None.")
+#entry.pack(side=BOTTOM)
+start_button = Button(root,text="Start",command=signal_interpreter)
+start_button.pack(side=LEFT)
+clear_button = Button(root,text="Clear",command=clear_textbox)
+clear_button.pack(side=RIGHT)
+
+#Label(root,text="Interpretation Period (Hz):").grid(row=2,column=7)
+#Entry(root).grid(row=3,column=7)
+#Button(root,text="Adjust").grid(row=3,column=8)
+#Label(root,text="History:").grid(row=6,column=7)
+#Entry(root).grid(row=7,column=7)
+#Button(root,text="Clear").grid(row=7,column=8)
+
+root.resizable(False,False)
+
+root.mainloop()
