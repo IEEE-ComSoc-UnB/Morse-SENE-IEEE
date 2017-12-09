d4c501a7cda53e4ce93cfd39a9d3b0fc8a333382
bruno-chaves <32115369+bruno-chaves@users.noreply.github.com>
2017-12-09 04:25:12 -0200

    Add files via upload

--- Codigos/main.py
+++ Codigos/main.py
@@ -2,8 +2,10 @@
 """
 Created on Sat Apr 22 10:41:22 2017
 
-@author: Calil
+@author: Bruno
 """
+
+import scanner
 
 morse={'01': 'a','1000': 'b','1010': 'c','100': 'd','0': 'e','0010': 'f',
        '110': 'g','0000': 'h','00': 'i','0111': 'j','101': 'k','0100': 'l',
@@ -11,66 +13,47 @@ morse={'01': 'a','1000': 'b','1010': 'c','100': 'd','0': 'e','0010': 'f',
        '000': 's','1': 't','001': 'u','0001': 'v','011': 'w','1001': 'x',
        '1011': 'y','1100': 'z','01111': '1','00111': '2','00011': '3','00001': '4',
        '00000': '5','10000': '6','11000': '7','11100': '8','11110': '9','11111': '0',}
+
+#simbolos é a string de traços e pontos
 simbolos=''
+
 def proc_sinais():
-    while True:
+
+    #x é a letra em questão
+    x=''
+    global simbolos
+    import scanner
+       
+    #inicialização
+    sample_rate = 2.048e6
+    gain = 49.6
     
-        
-        from pygame import mixer
-        
-        import scanner
-        
-        #mixer.init()
-        #mixer.music.load('F:\car_alarm_short.mp3')
-        
-        sample_rate = 2.048e6
-        gain = 49.6
-        
-        scn = scanner.Scanner(sample_rate,gain)
-        
-        center_freq = 433.9e6
-        thresh = 10
-        scn.start_monitor_psd(center_freq)
-        #print('apertado!')
-        #for i in scanner.lista:
-        #    print('{}\n'.format(i))
-        
-        if scanner.flag[0]==1:
-            for i in range(len(scanner.lista)):
-                if i%2==0:
-                    if scanner.lista[i]<0.5:
-                        simbolos=simbolos+'0'
-                    #if (i==0) and (scanner.lista[i]<0.5):
-                    #    simbolos=simbolos+'0'
-                    #elif (scanner.lista[i-1]>0.05) and (scanner.lista[i]<0.5):
-                    #    simbolos=simbolos+'0'
-                    else:
-                        simbolos=simbolos+'1'
-            #print('simbolos é: '+simbolos)
-            try:
-                x= morse[simbolos]
-                #print(morse[simbolos], end='')
-            except:
-                x= 'erro!'
-                #print('erro!')
-            #for i in range(int(len(simbolos)/3)):         
-            #    if simbolos[3*i:3*i+3]=='000':
-            #        print('s')
-            #    elif simbolos[3*i:3*i+3] =='111':
-            #        print('o')
-            #    else:
-            #        print('deu merda')
-            scanner.lista=[]
-            simbolos=''
-            scanner.flag[0]=0
-            return x
-                     
-        
-    #del scn
-    #while True:
-    #    x=scn.start_monitor_psd(center_freq,256,10,monit="MEAN")
-    #    sleep(0.2)
-    #    print('apertado!')
+    scn = scanner.Scanner(sample_rate,gain)
     
-    #mixer.music.play()
+    center_freq = 433.9e6
+    
+    scn.start_monitor_psd(center_freq)
+    
+    if scanner.flag[0]==1:
+        for i in range(len(scanner.lista)):
+            if scanner.lista[i]<0.6:
+                simbolos=simbolos+'0'
+            else:
+                simbolos=simbolos+'1'
+        try:
+            x=morse[simbolos]
+        except:
+            x='erro!'
+        scanner.lista=[]
+        simbolos=''
+        scanner.flag[0]=0
+        
+        scanner.letras=scanner.letras+x
+        print(x, end='')
+        
+    return x    
+
+                 
+while True:
+   proc_sinais()       
     
--- Codigos/scanner.py
+++ Codigos/scanner.py
@@ -13,6 +13,7 @@ from rtlsdr import RtlSdr
 import numpy as np
 import asyncio as asy
 import time
+import matplotlib.pyplot as plt
 
 lista = []
 t=[0]
@@ -26,115 +27,55 @@ class Scanner(object):
         self.sdr.sample_rate = sample_rate
         self.sdr.gain = gain
     
-    def plot_psd(self,fc,samp_scale=256):
-        if self.sdr.center_freq != fc:
-            self.sdr.center_freq = fc
-
-        samples = self.sdr.read_samples(samp_scale*1024)
-
-        # use matplotlib to estimate and plot the PSD
-        psd(samples, NFFT=1024, Fs=self.sdr.sample_rate/1e6, \
-            Fc=self.sdr.center_freq/1e6)
-        xlabel('Frequency (MHz)')
-        ylabel('Relative power (dB)')
-
-        show()
-        
-    def calc_psd(self,fc,samp_scale=256):
-        if self.sdr.center_freq != fc:
-            self.sdr.center_freq = fc
-
-        smpls = self.sdr.read_samples(samp_scale*1024)
-
-        # use matplotlib to estimate and plot the PSD
-        f, pow_sd = sig.welch(smpls,fs=self.sdr.sample_rate,nfft=1024,\
-                              nperseg = 1024,return_onesided = False)
-        
-        f = f + fc
-        
-        pow_db = 10*np.log10(pow_sd)
-        pow_db = pow_db - np.min(pow_db)
-        
-        return f, pow_db
-    
-    
     async def monitor_psd(self,fc,samp_scale,count_max,monit):
         if self.sdr.center_freq != fc:
             self.sdr.center_freq = fc
-        count = 0
         x=0
         
+        global lista
+        global flag
+        global t
+        
         async for smpls in self.sdr.stream():
-            count = count + 1
+
             
             f, pow_sd = sig.welch(smpls,fs=self.sdr.sample_rate,nfft=1024,\
                               nperseg = 1024,return_onesided = False)
             
             pow_db = 10*np.log10(pow_sd)
             pow_db = pow_db - np.min(pow_db)
+            #self.plot_psd(pow_db)
+            #xlabel('Frequency (MHz)')
+            #ylabel('Relative power (dB)')
             
-            #if monit == "MAX":
-            #    print(np.max(pow_db))
-            #elif monit == "MEAN":
-            #print(np.max(pow_db))
+            #show()
+                
             
-            if (np.max(pow_db)>30) and (x==0):
-                x=1
-                if lista !=[]:
-                   lista.append(time.time()-t[0]) 
+            if (np.max(pow_db)<20) and (x==2):
+                x=0
                 t[0]=time.time()
-                #print(t)
-            elif (np.max(pow_db)<30) and (x==1):
+                x=0
+            elif (np.max(pow_db)>20) and (x==0):
+                x=1
+                t[0]=time.time()
+            elif (np.max(pow_db)<20) and (x==1):
                 lista.append(time.time()-t[0])
                 t[0]=time.time()
                 x=0
-                #print(time.time()-t)
-                #print(lista)
-                #print('antes do stop')
                 self.sdr.stop()
-                #print('antes do close')
-                #self.sdr.close()
-                #print('depois do close')
-                #return
-            elif (time.time()-t[0]>1) and (lista!=[]) and (x==0):
-                #print('timeout')
+            elif (time.time()-t[0]>2) and (lista!=[]) and (x==0):
                 flag[0]=1
                 self.sdr.stop()
-        self.sdr.close()    
-            #if count > count_max:
-            #    self.sdr.stop()   
-        #print('done!')        
-        #self.sdr.close()
-        
-    async def monitor_psd_until(self,fc,thresh,samp_scale,monit):
-        if self.sdr.center_freq != fc:
-            self.sdr.center_freq = fc
-        
-        async for smpls in self.sdr.stream():
-            f, pow_sd = sig.welch(smpls,fs=self.sdr.sample_rate,nfft=1024,\
-                              nperseg = 1024,return_onesided = False)
-            
-            pow_db = 10*np.log10(pow_sd)
-            pow_db = pow_db - np.min(pow_db)
-            
-            if monit == "MAX":
-                val = np.max(pow_db)
-            elif monit == "MEAN":
-                val = np.mean(pow_db)
-                
-            #print(val)
-                
-            if val > thresh:
+            elif (time.time()-t[0]>5) and (x==1):
+                letras=''
+                lista=[]
+                print(chr(27) + "[2J")      #clear console
+                x=2
                 self.sdr.stop()
-                
-        self.sdr.close()
-        #print("Threshold reached!")
+        self.sdr.close()    
+
         
     def start_monitor_psd(self,fc,samp_scale = 256,count_max=10,monit = "MAX"):
         #print('antes do return')
         return asy.get_event_loop().run_until_complete(self.monitor_psd(fc,\
                           samp_scale,count_max,monit))
-        
-    def start_monitor_psd_until(self,fc,thresh,samp_scale = 256,monit = "MAX"):
-        return  asy.get_event_loop().run_until_complete(self.monitor_psd_until(fc,\
-                          thresh,samp_scale,monit))
